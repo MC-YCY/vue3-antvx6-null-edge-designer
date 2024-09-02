a11815f3d613b5e0e164faf8f36fa9fc794bb7c2
春秋半夏 <2646403766@qq.com>
2024-09-02 18:20:16 +0800

    fix:node components pointer-events: none;

--- src/components/designer/nodeList/address.vue
+++ src/components/designer/nodeList/address.vue
@@ -22,5 +22,6 @@ Node.on('change:data', ({current}) => {
   width: 100%;
   height: 100%;
   background: skyblue;
+  pointer-events: none;
 }
 </style>
--- src/components/designer/nodeList/box.vue
+++ src/components/designer/nodeList/box.vue
@@ -13,5 +13,6 @@
   width: 100%;
   height: 100%;
   background: red;
+  pointer-events: none;
 }
 </style>
